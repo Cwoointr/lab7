3bc65e02a1ad7008be41cd54695bb4481adc404a
Chonlada Woointranon <cwoointr@gmail.com>
2016-02-18 19:57:35 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(likeClicked);
+}
+
+function likeClicked(e){
+	e.preventDefault();
+	console.log("Like button clicked")
+	ga("send", "event", "like", "click");
 }
